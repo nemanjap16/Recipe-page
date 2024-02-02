27f902be97effe6d3c743a36562e755a12f5f99a
nemanjap16 <necaintruder@gmail.com>
2024-02-02 20:11:21 +0100

    done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        young: ["Young-serif", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+      colors: {
+        nutmeg: "hsl(14, 45%, 36%)",
+        "dark-raspberry": "hsl(332, 51%, 32%)",
+        white: "hsl(0, 0%, 100%)",
+        "rose-white": "hsl(330, 100%, 98%)",
+        eggshell: "hsl(30, 54%, 90%)",
+        "light-grey": "hsl(30, 18%, 87%)",
+        "wenge-brown": "hsl(30, 10%, 34%)",
+        "dark-charcoal": "hsl(24, 5%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+};
